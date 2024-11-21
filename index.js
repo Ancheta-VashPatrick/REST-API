caa0596279f1c2e84bd88912636f93c497558f4f
Rick Ancheta <ancheta.vashpatrick@gmail.com>
2024-11-21 12:42:22 +0800

    Secure API with Self-Signed HTTPS

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require("express");
+
+const https = require("https");
+const fs = require("fs");
+
 const app = express();
 const port = 3000;
+
 const sensorDataRouter = require("./routes/sensor-data");
+
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+
 app.get("/", (req, res) => {
   res.json({ message: "ok" });
 });
+
 app.use("/sensor-data", sensorDataRouter);
+
 /* Error handler middleware */
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -19,6 +28,14 @@ app.use((err, req, res, next) => {
   res.status(statusCode).json({ message: err.message });
   return;
 });
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+
+// Read SSL certificate and key files
+const options = {
+  key: fs.readFileSync("./localhost-key.pem"),
+  cert: fs.readFileSync("./localhost.pem"),
+};
+// Create HTTPS server
+const server = https.createServer(options, app);
+server.listen(port, () => {
+  console.log(`Example app listening at https://localhost:${port}`);
 });
--- links.md
+++ links.md
@@ -5,3 +5,4 @@
 [Main reference](https://blog.logrocket.com/build-rest-api-node-express-mysql/)
 
 [To secure with JWT](https://www.slingacademy.com/article/authentication-authorization-expressjs-jwt/)
+[Secured with HTTPS](https://dev.to/fredabod/building-an-express-app-with-an-https-server-2mbj)
